2fc0f4a2c97ccdf3401ef403bd819d7dcf490842
Alex Urbina <131113646+ualexander@users.noreply.github.com>
2024-09-04 14:03:27 -0400

    Create moveZeros.java

---
+++ moveZeros.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        // loop through array
+        for (int i = 0; i < nums.length; i++) {
+            // if encounter 0
+            if (nums[i] == 0 && i+1 < nums.length) {
+                // create innaer loop
+                int j = i;
+                // find next non-zero slot
+                while (nums[j] == 0 && j < nums.length - 1) {
+                    j++;
+                }
+                // swap
+                nums[i] = nums[j];
+                nums[j] = 0;
+            }
+        }
+    }
+}
